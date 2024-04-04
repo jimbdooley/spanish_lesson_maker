d7e0ced6e78a66c8719d41f9a67a7ad66d71c8f1
jimbdooley <jimbdooley@gmail.com>
2024-04-04 16:01:05 -0500

    storing wiki changes instead of added them to wiki object

--- public/lessonMaker/getUnchangedWordDef.js
+++ public/lessonMaker/getUnchangedWordDef.js
@@ -23,37 +23,42 @@ const addWikiKeyToLongDef = (() => {
     }
 })();
 
-function getWikiDef(defInfoObj) {
-    const freqInfo = A.typeFrequency.hasOwnProperty(defInfoObj.word)
-        ? A.typeFrequency[defInfoObj.word] : []
-    if (defInfoObj.shortDef == null) {
-        let firstKey = freqInfo.length > 0 ? freqInfo[0] : null
-        for (const key in A.wiki[defInfoObj.word]) {
-            firstKey = firstKey == null ? key : firstKey            
-            break
-        }
-        const shortDef = A.wiki[defInfoObj.word][firstKey][0][0]
-        defInfoObj.shortDef = removeWikiNewline(shortDef)
-        if (shortDef.length > 0 && shortDef[0] == "(" && shortDef.indexOf(")") != -1) {
-            if (shortDef.indexOf(")") + 4 < shortDef.length) {
-                defInfoObj.shortDef = defInfoObj.shortDef.substring(shortDef.indexOf(")") + 2)
+const getWikiDef = (() => {
+    const wikiShortDefs = {}
+    return function(defInfoObj) {
+        const freqInfo = A.typeFrequency.hasOwnProperty(defInfoObj.word)
+            ? A.typeFrequency[defInfoObj.word] : []
+        if (defInfoObj.shortDef == null) {
+            let firstKey = freqInfo.length > 0 ? freqInfo[0] : null
+            for (const key in A.wiki[defInfoObj.word]) {
+                firstKey = firstKey == null ? key : firstKey            
+                break
             }
+            if (!wikiShortDefs.hasOwnProperty(defInfoObj.word)) {
+                const shortDef = A.wiki[defInfoObj.word][firstKey][0][0]
+                wikiShortDefs[defInfoObj.word] = removeWikiNewline(shortDef)
+                if (shortDef.length > 0 && shortDef[0] == "(" && shortDef.indexOf(")") != -1) {
+                    if (shortDef.indexOf(")") + 4 < shortDef.length) {
+                        wikiShortDefs[defInfoObj.word] = defInfoObj.shortDef.substring(shortDef.indexOf(")") + 2)
+                    }
+                }
+            }
+            defInfoObj.shortDef = wikiShortDefs[defInfoObj.word]
+        }
+        if (defInfoObj.longDef == null) {
+            const longDef = []
+            for (const key of freqInfo) {
+                if (!(key in A.wiki[defInfoObj.word])) continue
+                addWikiKeyToLongDef(longDef, defInfoObj, key)
+            }
+            for (const key in A.wiki[defInfoObj.word]) {
+                if (-1 != freqInfo.indexOf(key)) continue
+                addWikiKeyToLongDef(longDef, defInfoObj, key)
+            }
+            defInfoObj.longDef = longDef
         }
     }
-    if (defInfoObj.longDef == null) {
-        const longDef = []
-        for (const key of freqInfo) {
-            if (!(key in A.wiki[defInfoObj.word])) continue
-            addWikiKeyToLongDef(longDef, defInfoObj, key)
-        }
-        for (const key in A.wiki[defInfoObj.word]) {
-            if (-1 != freqInfo.indexOf(key)) continue
-            addWikiKeyToLongDef(longDef, defInfoObj, key)
-        }
-        defInfoObj.longDef = longDef
-    }
-
-}
+})()
 
 function getUnchangedWordDef(defInfoObj) {
     if (A.myShortDefs.hasOwnProperty(defInfoObj.word)) {
