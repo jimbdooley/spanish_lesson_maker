64e85c13efb9ff306de38c75746750c0a12289be
jimbdooley <jimbdooley@gmail.com>
2024-04-07 13:06:11 -0600

    updated some words

--- pyScripts/applyLessonEdits.py
+++ pyScripts/applyLessonEdits.py
@@ -2,17 +2,20 @@ import os
 import json
 
 allDefsToDelete = [
-    ["Casilda (pt. 1).json", "toledo"],
+    ["Casilda (pt 1).json", "toledo", "mar\u00eda", "castillo"],
 ]
 
 def applyLessonEdits():
     for defsToDelete in allDefsToDelete:
-        with open(os.getcwd() + "/editedLessons/" + defsToDelete[0]) as json_file:
+        with open(os.getcwd() + "/editedLessons/" + defsToDelete[0], "r", encoding="utf8") as json_file:
             obj = json.load(json_file)
         for i in range(1, len(defsToDelete)):
-            obj["defInfo"].pop(defsToDelete[i])
+            if defsToDelete[i] in obj["defInfo"]:
+                obj["defInfo"].pop(defsToDelete[i])
+            if defsToDelete[i] in obj["genders"]:
+                obj["genders"].pop(defsToDelete[i])
 
-        with open(os.getcwd() + "/editedLessons/" + defsToDelete[0], "w") as json_file:
+        with open(os.getcwd() + "/editedLessons/" + defsToDelete[0], "w", encoding="utf8") as json_file:
             json.dump(obj, json_file, indent=1)
 
 applyLessonEdits()
